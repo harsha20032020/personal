0e187e24ce9cc05d6a1f51edb3c3e9c4b818a984
harsha20032020 <nemani.v@research.iiit.ac.in>
2021-07-03 07:01:14 +0530

    commited

--- js/main.js
+++ js/main.js
@@ -1,63 +1,17 @@
-$(document).ready(function () {
-
-
-    let $btns = $('.project-area .button-group button');
-
-
-    $btns.click(function (e) {
-
-        $('.project-area .button-group button').removeClass('active');
-        e.target.classList.add('active');
-
-        let selector = $(e.target).attr('data-filter');
-        $('.project-area .grid').isotope({
-            filter: selector
-        });
-
-        return false;
-    })
-
-    $('.project-area .button-group #btn1').trigger('click');
-
-    $('.project-area .grid .test-popup-link').magnificPopup({
-        type: 'image',
-        gallery: {
-            enabled: true
-        }
-    });
-
-
-    // Owl-carousel
-
-    $('.site-main .about-area .owl-carousel').owlCarousel({
-        loop: true,
-        autoplay: true,
-        dots: true,
-        responsive: {
-            0: {
-                items: 1
-            },
-            560: {
-                items: 2
-            }
-        }
-    })
-
-    // sticky navigation menu
-    let nav_offset_top = $('.header_area').height() + 50;
-
-    function navbarFixed() {
-        if ($('.header_area').length) {
-            $(window).scroll(function () {
-                let scroll = $(window).scrollTop();
-                if (scroll >= nav_offset_top) {
-                    $('.header_area .main-menu').addClass('navbar_fixed');
-                } else {
-                    $('.header_area .main-menu').removeClass('navbar_fixed');
-                }
-            })
-        }
+function change_image() {
+    var x = document.getElementById('scrollfn')
+    var str = document.getElementById('scrollfn').src;
+    if (str.includes("1.jpg")) {
+        x.src = "../img/2.JPG";
+    } else if (str.includes("2.JPG")) {
+        x.src = "../img/3.JPG";
+    } else if (str.includes("3.JPG")) {
+        x.src = "../img/4.JPG";
+    } else if (str.includes("4.JPG")) {
+        x.src = "../img/5.JPG";
+    } else if (str.includes("5.JPG")) {
+        x.src = "../img/1.jpg";
+    } else {
+        x.src = "";
     }
-
-    navbarFixed();
-});
+}
